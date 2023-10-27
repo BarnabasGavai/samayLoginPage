1bea153457a5d6feef8c3109b2922582efac49a0
BarnabasGavai <74435697+BarnabasGavai@users.noreply.github.com>
2023-10-27 14:22:25 +0530

    Add files via upload

---
+++ lib/Screens/RegisterScreen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:loginpage/Components/Login_button.dart';
+import 'package:loginpage/Components/Password_input.dart';
+import 'package:loginpage/Components/Titles.dart';
+import 'package:loginpage/Components/Username_input.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:loginpage/Components/Login_button.dart';
-import 'package:loginpage/Components/Password_input.dart';
-import 'package:loginpage/Components/Titles.dart';
-import 'package:loginpage/Components/Username_input.dart';
+
+import 'Screens/LoginScreen.dart';
+import 'Screens/RegisterScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -43,120 +41,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  String _email = "";
-  String _password = "";
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 6, 36, 10),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 80,
-              ),
-              Titles(title: "Login", subtitle: "Sign in to get started"),
-              SizedBox(
-                height: 80,
-              ),
-              Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Center(
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                "Username",
-                                style: GoogleFonts.inter(
-                                    color: Colors.white, fontSize: 14),
-                              ),
-                            ),
-                            UsernameInput(
-                                hint_text: "Username",
-                                validator: "Provide an email",
-                                bordercolor: Color.fromARGB(255, 117, 189, 65),
-                                on_saved: _email),
-                            SizedBox(height: 25),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                "Password",
-                                style: GoogleFonts.inter(
-                                    color: Colors.white, fontSize: 14),
-                              ),
-                            ),
-                            Password_inputfield(
-                              on_saved: _password,
-                              bordercolor:
-                                  const Color.fromARGB(255, 117, 189, 65),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Container(
-                              padding: EdgeInsets.fromLTRB(0, 10, 10, 0),
-                              alignment: Alignment.centerRight,
-                              child: Text(
-                                "Forgot Password?",
-                                style: GoogleFonts.inter(
-                                    fontSize: 14,
-                                    color: Color.fromARGB(255, 254, 250, 250)),
-                              ),
-                            )
-                          ]),
-                    ),
-                    SizedBox(height: 60),
-                    Center(
-                        child: Column(
-                      children: [
-                        SizedBox(
-                          height: 10,
-                        ),
-                        LoginButton(
-                            button_name: "Login",
-                            onclick_func: () {
-                              _formKey.currentState!.validate();
-                            }),
-                        SizedBox(
-                          height: 25,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Don't have an account? ",
-                              style: GoogleFonts.inter(
-                                  fontSize: 14,
-                                  color: Color.fromARGB(255, 254, 250, 250)),
-                            ),
-                            Text(
-                              "Sign up",
-                              style: GoogleFonts.inter(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 14),
-                            )
-                          ],
-                        )
-                      ],
-                    )),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+    return LoginScreen();
   }
 }
